15bf0eedbc03aef123b57a3cd6102e7fa81bb245
ibogdanssh <ibogdanssh@gmail.com>
2025-01-20 19:59:17 +0200

    Update main page
    Update the main page design and add new features

--- src/Controller/MainController.java
+++ src/Controller/MainController.java
@@ -5,6 +5,8 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.paint.ImagePattern;
 import javafx.scene.shape.Circle;
+import Controller.Controller;
+import javafx.scene.control.Label;
 
 public class MainController {
 
@@ -13,4 +15,12 @@ public class MainController {
 
     @FXML
     private Circle clipCircle;
+
+    @FXML
+    private Label nameLabel;
+
+    public void initialize() {
+        System.out.println(Controller.username);
+        nameLabel.setText(Controller.username.substring(0, 1).toUpperCase() + Controller.username.substring(1));
+    }
 }
